ba644bf8ab763748953a05cff3da777c10e7b814
Zulqarnayeen Sadid <zulqarnayeensadid@gmail.com>
2026-06-25 12:57:08 -0400

    Refactor Supabase client initialization for browser

--- Project/book_it/lib/supabase.js
+++ Project/book_it/lib/supabase.js
@@ -1,8 +1,10 @@
-import { createClient } from "@supabase/supabase-js"
-
-const supabase = createClient(
-    process.env.NEXT_PUBLIC_PROJECT_URL,
-    process.env.NEXT_PUBLIC_ANON_KEY
-)
-
-export default supabase
+import { createBrowserClient } from "@supabase/ssr"
+let client = null
+export function getSupabaseBrowser() {
+    if (client) return client
+    client = createBrowserClient(
+        process.env.NEXT_PUBLIC_PROJECT_URL,
+        process.env.NEXT_PUBLIC_ANON_KEY
+    )
+    return client
+}
